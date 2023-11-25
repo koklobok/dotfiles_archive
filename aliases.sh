889f242d1ef12a541e333bd1f2bb89823f12ef87
Roman Holiuk <roman.golyuk@gmail.com>
2023-11-25 12:11:09 +0200

    update aliases

--- aliases.sh
+++ aliases.sh
@@ -3,3 +3,6 @@ alias di='podman images'
 alias dip='podman image prune -f'
 alias drun='podman run --rm -it'
 alias dexec='podman exec -it'
+alias pprune='podman image prune -f && podman container prune -f'
+
+alias ii='sudo dnf install -y'
